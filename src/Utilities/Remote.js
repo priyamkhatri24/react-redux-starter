6d9235d43e20636d70416c12b2afe27c6f5c3183
priyamkhatri24 <68660957+priyamkhatri24@users.noreply.github.com>
2021-12-23 18:05:52 +0530

    commit before merge

--- src/Utilities/Remote.js
+++ src/Utilities/Remote.js
@@ -52,7 +52,8 @@ export const post = (requestBody, endpoint) => {
 
 export const get = (requestBody = null, endpoint) => {
   // requestBody must be an object
-  return axios.get(testUrl + endpoint, { params: requestBody, headers: authHeaderGet() })
+  return axios
+    .get(testUrl + endpoint, { params: requestBody, headers: authHeaderGet() })
     .then((result) => result.data)
     .catch((err) => {
       history.push('/error');
